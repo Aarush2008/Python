4d879ee2bf0379e4324a778a401359ccb2b51768
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2022-04-20 21:22:03 -0600

    Lesson 11

---
+++ Lesson_11-Tuples.py
@@ -0,0 +1,8 @@
+# Type of data structure which stores different values or information
+coordinates = (4, 5)
+# Tuple are usally shown as coordinates
+# Tuple is inmutable, it cant be changed or modified.
+print(coordinates[0])
+# 4 is in index position 0 while  is in 1
+# In lists you can substitue the value as something else and you use square brackets while in tuples you cant change anything and you use normal brackets
+coordinates = (4, 5), (6, 7), (60, 34)
